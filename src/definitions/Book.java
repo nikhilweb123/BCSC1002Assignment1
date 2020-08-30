72df1858096cbb318621d89ce4b7a7a3644f3daf
nikhil kumar <nikhil.kumar_cs19@gla.ac.in>
2020-08-30 13:27:53 +0530

    commit Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,19 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: nikhil kumar (191500499)
+ *  Date: 29/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
 package definitions;
 
 public class Book {
+    private string nameOfBook;
+    private string authorOfBook;
+    private string isNumberOfBook;
+
+    public Book(String nameOfBook,String authorOfBook,String isNumberOfBook)
+
+
+
+
 }
