149376ac3348c895115ce16b91b4f3ed442db94a
nikhil kumar <nikhil.kumar_cs19@gla.ac.in>
2020-08-30 20:10:00 +0530

    commit FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,7 +6,16 @@
  * */
 package execution;
 
+import definitions.Library;
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
+    private static final int ISSUE_A_NEW_BOOK = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
     public static void main(String[] args) {
 
     }
