16276b8f10976c7661c8d5212dff0164f0b3a8fc
nikhil kumar <nikhil.kumar_cs19@gla.ac.in>
2020-08-30 20:13:44 +0530

    commit FrontDesk.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -9,21 +9,33 @@ package definitions;
 import java.util.Arrays;
 
 public class Library {
-    private Book[] booksCurrentlyAvailable;
+    private static final int MAXIMUM_BOOKS_IN_LIBRARY = 5;
+    private Book[] booksThatAreCurrentlyAvailable;
 
+    // Parameterized Constructor
+    public Library(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
+
+    // Non-Parameterized Constructor
     public Library() {
-        this.booksCurrentlyAvailable = new Book[20];
-        for (int index = 0; index < booksCurrentlyAvailable.length; index++) {
-            booksCurrentlyAvailable[index] = new Book();
+        this.booksThatAreCurrentlyAvailable = new Book[MAXIMUM_BOOKS_IN_LIBRARY];
+        for (int libraryIndex = 0; libraryIndex < MAXIMUM_BOOKS_IN_LIBRARY; libraryIndex++) {
+            this.booksThatAreCurrentlyAvailable[libraryIndex] = new Book();
         }
     }
 
-    public Book[] getBooksCurrentlyAvailable() {
-        return booksCurrentlyAvailable.clone();
+    public Book[] getBooksThatAreCurrentlyAvailable() {
+        return booksThatAreCurrentlyAvailable;
     }
 
-    public void setBooksCurrentlyAvailable(Book[] booksCurrentlyAvailable) {
-        this.booksCurrentlyAvailable = booksCurrentlyAvailable;
+    public void setBooksThatAreCurrentlyAvailable(Book[] booksThatAreCurrentlyAvailable) {
+        this.booksThatAreCurrentlyAvailable = booksThatAreCurrentlyAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(booksThatAreCurrentlyAvailable);
     }
 
     @Override
@@ -31,21 +43,45 @@ public class Library {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Library library = (Library) o;
-        return Arrays.equals(getBooksCurrentlyAvailable(), library.getBooksCurrentlyAvailable());
+        return Arrays.equals(getBooksThatAreCurrentlyAvailable(), library.getBooksThatAreCurrentlyAvailable());
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(getBooksCurrentlyAvailable());
+        return Arrays.hashCode(getBooksThatAreCurrentlyAvailable());
     }
 
     /**
-     * This method will show the list of books currently available in the library.
+     * This method will set the Information of books available in Library.
      */
-    public void showListOfBooks() {
-        for (Book book : booksCurrentlyAvailable) {
-            System.out.println("List of books:");
-            System.out.println(book);
+    public void setBooksInLibrary() {
+        booksThatAreCurrentlyAvailable[0].setNameOfTheBook("Engineering Mathematics");
+        booksThatAreCurrentlyAvailable[0].setNameOfTheAuthorOfTheBook("H. K. Das");
+        booksThatAreCurrentlyAvailable[0].setThirteenDigitISBNNumberOfTheBook("9352836537000");
+        booksThatAreCurrentlyAvailable[1].setNameOfTheBook("Digital Electronics");
+        booksThatAreCurrentlyAvailable[1].setNameOfTheAuthorOfTheBook("M. Mano");
+        booksThatAreCurrentlyAvailable[1].setThirteenDigitISBNNumberOfTheBook("9353062012000");
+        booksThatAreCurrentlyAvailable[2].setNameOfTheBook("Database Management System");
+        booksThatAreCurrentlyAvailable[2].setNameOfTheAuthorOfTheBook("Navathe");
+        booksThatAreCurrentlyAvailable[2].setThirteenDigitISBNNumberOfTheBook("9789332582705");
+        booksThatAreCurrentlyAvailable[3].setNameOfTheBook("Fundamental of Software Engineering");
+        booksThatAreCurrentlyAvailable[3].setNameOfTheAuthorOfTheBook("Rajib Mall");
+        booksThatAreCurrentlyAvailable[3].setThirteenDigitISBNNumberOfTheBook("9789388028028");
+        booksThatAreCurrentlyAvailable[4].setNameOfTheBook("Core JAVA");
+        booksThatAreCurrentlyAvailable[4].setNameOfTheAuthorOfTheBook("Dr. R. Nageswara Rao");
+        booksThatAreCurrentlyAvailable[4].setThirteenDigitISBNNumberOfTheBook("9351199258111");
+    }
+
+    /**
+     * This method will show the Information of books available in Library.
+     */
+    public void showAvailableBooks() {
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.printf("|%-36s %-21s %-21s|\n", "Book Name", "Author Name", "13-Digit ISBN Number");
+        System.out.println("----------------------------------------------------------------------------------");
+        for (int libraryIndex = 0; libraryIndex < MAXIMUM_BOOKS_IN_LIBRARY; libraryIndex++) {
+            System.out.printf("|%-36s %-21s %-21s|\n", booksThatAreCurrentlyAvailable[libraryIndex].getNameOfTheBook(), booksThatAreCurrentlyAvailable[libraryIndex].getNameOfTheAuthorOfTheBook(), booksThatAreCurrentlyAvailable[libraryIndex].getThirteenDigitISBNNumberOfTheBook());
         }
+        System.out.println("----------------------------------------------------------------------------------");
     }
 }
